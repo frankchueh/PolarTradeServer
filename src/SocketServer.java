4abe94604e4917f7b4545c68899ab84fbdd9b283
yucheng7713 <stjobiwtc@gmail.com>
2015-11-19 11:12:36 +0800

    server first commit

---
+++ src/SocketServer.java
@@ -0,0 +1,431 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+import java.util.*;
+
+
+public class SocketServer {
+
+	userdb DBuser = new userdb();
+    locationDB DBmap = new locationDB();
+    productDB DBproduct = new productDB();
+    chatdb DBchat = new chatdb();
+	public void run() {
+
+		MessageRecevicer mr = new MessageRecevicer();
+		mr.start();
+	}
+	
+
+	public static void main(String args[]) {
+
+		InetAddress ip = null;
+		String hostname;
+		try {
+			ip = InetAddress.getLocalHost();
+			hostname = ip.getHostName();
+			System.out.println("IP:" + ip + "\nname:" + hostname);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		(new SocketServer()).run();
+		
+
+	}
+
+	class MessageRecevicer extends Thread {
+
+		private ServerSocket socket1;
+		private Socket conn1;
+		private final int Port1 = 3838;
+
+		public void run() {
+			try {
+				socket1 = new ServerSocket(Port1);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			int threadNo;
+			for (threadNo = 1;; threadNo++) {
+				System.out.println("MessageRecevicer waiting...");
+				String conIp = "";
+				try {
+					conn1 = socket1.accept();
+					conIp = conn1.getInetAddress().toString();
+					System.out.println("MessageRecevicer: receive " + conIp
+							+ " calling....");
+					conn1.setSoTimeout(15000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				System.out.println("MessageRecevicer: Thread " + threadNo
+						+ "handling");
+				MessageReceviceThread newMessageThread = new MessageReceviceThread(
+						conn1, threadNo);
+				newMessageThread.start();
+
+			}
+		}
+	}
+
+
+	class MessageReceviceThread extends Thread {
+		private Socket conn;
+		int threadNo;
+		FileManager fileMgr;
+
+		public MessageReceviceThread(Socket conn, int threadNo) {
+			this.conn = conn;
+			this.threadNo = threadNo;
+
+			System.out.println("MessageReceviceThread " + threadNo + " handle "
+					+ conn.getInetAddress() + "'s calling");
+		}
+
+		public void run() {
+			try {
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						conn.getInputStream()));
+				
+				String command;
+				command = br.readLine(); // 第一行為指令
+
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(
+						conn.getOutputStream(), "utf-8"), true);
+				
+				//判斷指令是什麼
+				if (command.equals("Login")) { // 登入指令,會回傳是否成功
+					if (DBuser.Login(br.readLine(), br.readLine())) {
+						pw.println("success");
+						System.out.println("login success");
+					} else {
+						pw.println("fail");
+						System.out.println("login fail");
+					}
+				} else if (command.equals("SignUp")) { // 建立帳戶指令,會回傳是否成功(帳戶是否已存在)
+					String account = br.readLine();
+					String password = br.readLine();
+					String username = br.readLine();
+					if (DBuser.SignUp(account, password, username)) {
+						System.out.println("Sign up success");
+						ObjectInputStream ois = new ObjectInputStream(
+								conn.getInputStream());
+						byte[] buffer;
+						try {
+							if ((buffer = (byte[]) ois.readObject()) != null) // 判斷是否有照片
+							{
+								FileManager photo = new FileManager(
+										"UserPhoto/" + account + ".jpg");
+								photo.writeObjec(buffer);
+								DBuser.setPhoto(account, photo.savePath); // 變更使用者照片
+							}
+						} catch (ClassNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						pw.println("success");
+					} else {
+						System.out.println("Sign up fail"); // 帳戶已存在
+						pw.println("fail");
+					}
+				}
+
+				else if (command.equals("GetUserInfo")) {
+					String account = br.readLine();
+					String userInfo = DBuser.getUserInfo(account);
+					int userID = DBuser.getUserID(account);
+					String userDetail = DBuser.getUserDetail(userID);
+
+					if (userInfo != null) {
+						pw.println("success");
+						pw.println(userInfo);
+						pw.println(userDetail);
+					} else {
+						pw.print("fail");
+					}
+				}
+
+				else if (command.equals("GetPhoto")) {
+					String photoPath = br.readLine();
+					File f = new File(photoPath);
+					if (f.exists()) {
+						pw.println("success");
+						System.out.println("檔案存在");
+
+						FileInputStream fis = new FileInputStream(f);
+						ObjectOutputStream oos = new ObjectOutputStream(
+								conn.getOutputStream());
+						byte[] buffer = new byte[1024];
+						int len = -1;
+						ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+						while ((len = fis.read(buffer)) != -1) {
+							outStream.write(buffer, 0, len);
+						}
+						byte[] photo = outStream.toByteArray();
+						oos.writeObject(photo);
+						fis.close();
+
+					} else {
+						pw.println("fail");
+						System.out.println("檔案不存在");
+					}
+
+				} else if (command.equals("UpdateUserInfo")) {
+					String account = br.readLine();
+					String username = br.readLine();
+					String age = br.readLine();
+					String birth = br.readLine();
+					String sex = br.readLine();
+					String phone = br.readLine();
+					String email = br.readLine();
+					
+					
+					if (DBuser.updateUserinfo(username, account)) {
+						int userID = DBuser.getUserID(account);
+						if (DBuser.updateUserDetail(userID,
+										Integer.parseInt(age), birth, sex,
+										phone, email)) {
+							pw.println("success");
+						}
+					} else
+						pw.println("fail");
+				}
+				else if (command.equals("UpdateUserPhoto")) {
+					
+					String account=br.readLine();
+					pw.println("OK");
+					ObjectInputStream ois = new ObjectInputStream(conn.getInputStream());
+					byte[] buffer;
+					try {
+						if ((buffer = (byte[]) ois.readObject()) != null) // 判斷是否有照片
+						{
+							FileManager photo = new FileManager(
+									"UserPhoto/" + account + ".jpg");
+							photo.writeObjec(buffer);
+							DBuser.setPhoto(account, photo.savePath); // 變更使用者照片
+							pw.println("success");
+						}
+					}
+					
+					catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						pw.println("fail");
+					}
+					}
+				
+				else if(command.equals("DownloadMessage"))
+				{
+					int chatID = Integer.parseInt(br.readLine());
+					int userID = Integer.parseInt(br.readLine());
+					DBchat.cancelNotificatiom(chatID, userID);
+					FileManager chatData = new FileManager("chatroom/"+chatID+".txt");
+					String[] tem_data = chatData.readAllLine();
+					String data="";
+					for(int i=0;i<tem_data.length;i++)
+					{
+						data += tem_data[i];
+					}
+					pw.println("success");
+					pw.println(data);
+					
+				}
+				
+				else if(command.equals("GetChatRoom"))
+				{
+					int PID = Integer.parseInt(br.readLine());
+					int SID = Integer.parseInt(br.readLine());
+					int BID = Integer.parseInt(br.readLine());
+					int chatID = DBchat.getChatroom(PID, SID, BID);
+					if(chatID!=-1)
+					{
+						pw.println("success");
+						pw.println(chatID);
+					}
+					else
+						pw.println("fail");
+					
+				}
+				
+				else if(command.equals("UpdateMessage"))
+				{
+					int chatID = Integer.parseInt(br.readLine());
+					int userID = Integer.parseInt(br.readLine());
+					DBchat.sendNotification(chatID, userID);
+					FileManager chatData = new FileManager("chatroom/"+chatID+".txt");
+					String line;
+					while((line=br.readLine())!=null)
+					{
+						chatData.writeLine(line);
+					}
+					pw.println("success");
+				}
+				
+				else if(command.equals("ListChatRoom"))
+				{
+					String Account = br.readLine();
+					int userID = DBuser.getUserID(Account);
+					String B = DBchat.getChatroomForBuyer(userID);
+					String S = DBchat.getChatroomForSeller(userID);
+					pw.println("success");
+					pw.println(B);
+					pw.println(S);
+				}
+				else if(command.equals("updateLocate")) {
+					
+					int userID = DBuser.getUserID(br.readLine());
+					if(DBmap.uploadLocation(userID,
+											Double.parseDouble(br.readLine()),
+											Double.parseDouble(br.readLine()))) {
+						pw.println("success");
+						System.out.println("location upload success");
+					}
+					else {
+						pw.println("failed");
+						System.out.println("location upload failed");
+					}
+				}
+				else if(command.equals("InsertProduct")) {
+					//System.out.println("Product start insert");
+
+					String productInfo = "";
+					String [] s_productInfo;
+					int productID;
+					
+					String productName = br.readLine();   // 讀取  product 名稱
+					int productPrice = Integer.parseInt(br.readLine());  // 讀取 product 金額
+					int userID = DBuser.getUserID(br.readLine());  // 讀取使用者ID
+					 
+					pw.println("msg1 success");
+					
+					productID = DBproduct.insertProduct(productName,productPrice,userID); 
+					if(productID != -1) {  // 若 insert 成功 -> 建立商品路徑 ( 用來存 photo , info )
+						
+						try {
+							File product_dir = new File("C:/DataBase/product/"+ productID);
+							product_dir.mkdir();
+						}catch(Exception e) {
+							System.out.println("Create directory failed");
+							e.printStackTrace();
+						}
+				
+						ObjectInputStream ois = new ObjectInputStream(conn.getInputStream());
+						byte[] buffer;
+						
+						try {
+							if((buffer=(byte[])ois.readObject())!=null)	//判斷是否有 product info傳來
+							{
+				 
+								FileManager info = new FileManager("/product/"+ productID +"/" + "info.txt");
+								productInfo = new String(buffer,StandardCharsets.UTF_8);
+								s_productInfo = productInfo.split("\n");
+								info.writeAllLine(s_productInfo);;  // 將product  info存到指定路徑
+								
+								pw.println("msg2 success");
+								pw.println(productID);
+							}
+						} catch (ClassNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}							
+					}
+					else {
+						System.out.println("Product insert failed!!");
+						pw.println("upload failed");
+					}
+					
+				}
+				else if(command.equals("uploadProductPhoto")) {
+					
+					int userID = DBuser.getUserID(br.readLine());
+					pw.println("msg1 success");
+					
+					int productID = DBproduct.getNewProductIDbyUserID(userID);  // 重新根據 userID 取得 productID
+					
+					ObjectInputStream ois = new ObjectInputStream(conn.getInputStream());
+					byte[] buffer;
+					
+					try {
+						if((buffer=(byte[])ois.readObject())!=null)	//判斷是否有 product photo 傳來
+						{
+							FileManager photo = new FileManager("/product/"+ productID + "/" + "photo.jpg");
+							photo.writeObjec(buffer);  // 將照片存到指定路徑
+							
+							pw.println("msg2 success");
+						}
+					} catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}	
+				}
+				else if(command.equals("getUserProduct")) {    // 取得使用者的所有商品ID
+					int userID = DBuser.getUserID(br.readLine());
+					String pid_set = "";
+					
+					pid_set = DBproduct.getUserProduct(userID);
+					pw.println("success");
+					pw.println(pid_set);
+					
+					//System.out.println("Get pid success :" + pid_set);
+				}
+				else if(command.equals("getProductInfo")) {
+					int productID = Integer.parseInt(br.readLine());
+					
+					String pinfo = DBproduct.getProductInfo(productID);
+					
+					if(pinfo!=null)
+					{	
+						pw.println("success");
+						pw.println(pinfo);
+						
+						//System.out.println("Get pinfo : " + pinfo);
+					}
+					else
+					{
+						pw.print("fail");
+						
+						//System.out.println("Get all pinfo fail");
+					}
+				}
+
+				pw.close();
+//				try {
+//					Thread.sleep(3000);
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+
+				conn.close();
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	
+}
---
+++ src/productDB.java
@@ -0,0 +1,236 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.*;
+
+
+public class productDB {
+	
+	// Database 物件
+	private Connection dbConnect = null;
+	// 執行後傳入SQL的字串
+	private Statement input_stat = null;
+	// Query　結果
+	private ResultSet result = null;
+	// 執行後傳入 SQL的預備字串 -> 等待給定數值 -> 用　"?" 做標示
+	private PreparedStatement prepare_input_stat = null;
+	
+	String Qresult;
+	
+	
+	// Database 連線
+	public productDB() {
+		
+		try {
+			// Driver 註冊
+			Class.forName("com.mysql.jdbc.Driver");
+			// 取得 Connection -> 給定主機名稱 ，Database 名稱  ，使用者登入資訊
+			dbConnect = DriverManager.getConnection("jdbc:mysql://localhost:8038/schoolproject?useUnicode=true&characterEncoding=Big5", 
+											  "root", 
+											  "steveandfrank");
+			
+		}catch(ClassNotFoundException e) {
+			System.out.println("DriverClassNotFound :" + e.toString());
+		}
+		catch(SQLException x) {
+			System.out.println("Exception :" + x.toString());
+		}
+	}
+	
+	// 根據 userID 取得其所有的 productID 
+	
+	public String getUserProduct(int userID) {
+		
+	    Qresult = "";
+	    String query = "SELECT productID FROM productdb WHERE userid = ? AND state != \"deleted\";";
+	    
+	    try {
+	    	prepare_input_stat = dbConnect.prepareStatement(query);
+			prepare_input_stat.setInt(1, userID);
+			result = prepare_input_stat.executeQuery();
+	    	
+	    	while(result.next()) {
+	    		
+	    		Qresult += result.getInt("productID") + ",";
+	    	}
+	    }catch(SQLException e) {
+			System.out.println("SelectDB Exception :" + e.toString());
+		}finally {
+			close();
+		}
+		
+		return Qresult;
+	}
+	
+	// 根據 productID 取得其所有的相關資訊 ( 名稱 , 價格  , 照片路徑 , 資訊路徑 )
+	
+	public String getProductInfo(int productID) {
+		
+		Qresult = "";
+		String query = "SELECT * FROM productdb WHERE productID = ?;";
+		
+		try {
+	    	prepare_input_stat = dbConnect.prepareStatement(query);
+			prepare_input_stat.setInt(1, productID);
+			result = prepare_input_stat.executeQuery();
+	    	
+			if(result.next()){
+				Qresult += result.getString("Pname") + "," +
+						   result.getInt("price") + "," +
+						   result.getString("Pphoto") + "," +
+						   result.getString("Pinfo");
+			}
+	    			   
+	    }catch(SQLException e) {
+			System.out.println("SelectDB Exception :" + e.toString());
+		}finally {
+			close();
+		}
+		
+		return Qresult;
+
+	}
+	
+	// 更新商品相關資訊 ( 名稱 , 價格 )
+	
+	public void updateProduct(int productID , String Pname , int price) {
+		
+		String query = "UPDATE productdb SET Pname = ? , price = ? WHERE productID = ?;";
+		
+		try{
+			prepare_input_stat = dbConnect.prepareStatement(query);
+			prepare_input_stat.setString(1, Pname);
+			prepare_input_stat.setInt(2, price);
+			prepare_input_stat.setInt(3, productID);
+			prepare_input_stat.executeUpdate();
+			
+		}catch(SQLException e) {
+			System.out.println("UpdateDB Exception :" + e.toString());
+		}finally {
+			close();
+		}
+	}
+	
+	// 新增一筆新的商品資訊
+	
+	public int insertProduct(String Pname , int price , int userID) {
+		
+		String insertQuery = "INSERT INTO productdb(productID,Pname,price,userID,state)"
+				+ "SELECT IFNULL(max(productID),0)+1 , ? , ? , ? , ? FROM productdb;";
+		
+		String selectQuery = "SELECT max(productID) FROM productdb WHERE userID = ?;";
+		String updateQuery = "UPDATE productdb SET Pphoto = ? , Pinfo = ? WHERE productID = ?;";
+		
+		int newProductID = -1;  // 初始值為 -1 
+		
+		try{
+			prepare_input_stat = dbConnect.prepareStatement(insertQuery);
+			prepare_input_stat.setString(1, Pname);
+			prepare_input_stat.setInt(2, price);
+			prepare_input_stat.setInt(3, userID);
+			prepare_input_stat.setString(4, "new");
+			prepare_input_stat.executeUpdate();
+			
+			try{
+				prepare_input_stat = dbConnect.prepareStatement(selectQuery);
+				prepare_input_stat.setInt(1,userID);
+				result = prepare_input_stat.executeQuery();
+				
+				if(result.next()) {
+					newProductID = result.getInt("max(productID)");
+				}
+				
+				String path = "C:/DataBase/product/" + newProductID + "/";
+				
+				try{
+					prepare_input_stat = dbConnect.prepareStatement(updateQuery);
+					prepare_input_stat.setString(1,path + "photo.jpg");
+					prepare_input_stat.setString(2,path + "info.txt");
+					prepare_input_stat.setInt(3,newProductID);
+					prepare_input_stat.executeUpdate();
+				}catch(SQLException e) {
+					System.out.println("UpdateDB Exception :" + e.toString());
+				}
+				
+			}catch(SQLException e) {
+				System.out.println("SelectDB Exception :" + e.toString());
+			}
+			
+		}catch(SQLException e) {
+			System.out.println("InsertDB Exception :" + e.toString());
+		}finally {
+			close();
+		}
+		
+		System.out.println("product insert success");
+		
+		return newProductID;
+	}
+	
+	// 根據 productID 去刪除商品
+	
+	public void deletProduct(int productID) {
+		
+		String query = "UPDATE productdb SET state = \"deleted\" WHERE productID = ?;";
+		
+		try{
+			prepare_input_stat = dbConnect.prepareStatement(query);
+			prepare_input_stat.setInt(1, productID);
+			prepare_input_stat.executeUpdate();
+			
+		}catch(SQLException e) {
+			System.out.println("UpdateDB Exception :" + e.toString());
+		}finally {
+			close();
+		}
+	}
+	
+	// 新增商品圖片時，根據 userID 去取得其最新 (max)的 productID
+	
+	public int getNewProductIDbyUserID(int userID) {
+		
+		String selectQuery = "SELECT max(productID) FROM productdb WHERE userID = ?;";
+		int photoProductID = -1;
+		try {
+			prepare_input_stat = dbConnect.prepareStatement(selectQuery);
+			prepare_input_stat.setInt(1,userID);
+			result = prepare_input_stat.executeQuery();
+			
+			if(result.next()) {
+				photoProductID = result.getInt("max(productID)");
+			}
+			
+		}catch(SQLException e) {
+			System.out.println("SelectDB Exception :" + e.toString());
+		}
+		
+		return photoProductID;
+	}
+	
+	
+	// Close 方法
+	
+	private void close() {
+				
+		try{
+			if(result != null){
+				result.close();
+				result = null;
+			}
+			if(input_stat != null){
+				input_stat.close();
+				input_stat = null;
+			}
+			if(prepare_input_stat != null){
+				prepare_input_stat.close();
+				prepare_input_stat = null;
+			}
+		}catch(SQLException e) {
+			System.out.println("Close Exception :" + e.toString());
+		}
+	}
+}
