08526131baf5825327c236418533c15f2b308cb7
Frankchueh <as8307044@gmail.com>
2015-12-12 22:10:13 +0800

    add variable userID

--- src/com/example/project_ver1/Product.java
+++ src/com/example/project_ver1/Product.java
@@ -10,12 +10,14 @@ public class Product implements Serializable{
 	int productPrice;
 	byte[] productInfo;
 	byte[] productPhoto;
+	int userID;
 	
-	Product(int pID , String pName, int pPrice , byte[] pInfo , byte[] pPhoto) {
+	Product(int pID , String pName, int pPrice , byte[] pInfo , byte[] pPhoto, int pUserID) {
 		this.productID = pID;
 		this.productName = pName;
 		this.productPrice = pPrice;
 		this.productInfo = pInfo;
 		this.productPhoto = pPhoto;
+		this.userID = pUserID;
 	}
 }
